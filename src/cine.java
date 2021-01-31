564bf4863069b0c0ff463dc1d440333357320b98
Daniels-CIDE <73656233+Daniels-CIDE@users.noreply.github.com>
2021-01-31 14:58:53 +0100

    Final

--- src/cine.java
+++ src/cine.java
@@ -63,8 +63,8 @@ public class cine {
         double parteEntera, resultado;
         resultado = valorInicial;
         parteEntera = Math.floor(resultado);
-        resultado=(resultado-parteEntera)*Math.pow(10, numeroDecimales); //10 es la base y el numero de decimales es el exponente
-        resultado=Math.round(resultado); //Devuelve el número redondeado al mas cercano
+        resultado=(resultado-parteEntera)*Math.pow(10, numeroDecimales);
+        resultado=Math.round(resultado);
         resultado=(resultado/Math.pow(10, numeroDecimales))+parteEntera;
         return resultado;
     }
